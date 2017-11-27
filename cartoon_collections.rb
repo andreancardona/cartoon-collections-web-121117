31e91fd2fff23ccf2fc6a74fcba7d981785d6860
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-27 14:06:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,5 @@ def find_the_cheese(cheese)
   # the array below is here to help
    cheese_types = ["cheddar", "gouda", "camembert"]
    cheese.find do |cheese|
-   cheese_types.include?(cheese)
  end
 end
