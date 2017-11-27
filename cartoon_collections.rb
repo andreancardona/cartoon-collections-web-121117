81617c14ebe394ec9e0ce38861816c71b8bd8f9f
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-27 13:16:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(array)
 
 def summon_captain_planet(array)
       array.collect do |elements|
-      return elements.capitalize + "!"
+      elements.capitalize + "!"
     end
 end
 
