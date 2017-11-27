fd5f784a60fbee5a4330fef2e384a63f2f51d5d6
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-27 13:13:26 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,10 +4,12 @@ def roll_call_dwarves(array)
      end
   end
 
-def summon_captain_planet(array)
-  array.collect do | element |
-    return element.capitalize + "!"
-end
+def summon_captain_planet(veggies)# code an argument here
+    # Your code here
+    # planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+      veggies.collect do |call|
+      call.capitalize + "!"
+    end
 end
 
 def long_planeteer_calls# code an argument here
